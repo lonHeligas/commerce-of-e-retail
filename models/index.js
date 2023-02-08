2576db6a19b8cc535f0c5eaa3270f403f736239c
Lon Heligas <lon.heligas@gmail.com>
2023-02-07 18:07:05 -0600

    testing the seeding

--- models/index.js
+++ models/index.js
@@ -5,13 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  }
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  }
+});
 module.exports = {
   Product,
   Category,
