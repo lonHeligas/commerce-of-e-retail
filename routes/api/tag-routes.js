ffed47c7f2433f7e31d82529ea158333a64dd43c
Lon Heligas <lon.heligas@gmail.com>
2023-02-08 22:35:27 -0600

    added tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,29 +38,37 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-
+    const tagData = await Tag.create({ tag_name: req.body.tag_name
+    })
+    res.json(tagData)
 
   } catch (error) {
     console.log(error);
     res.status(400).json(error);
   }
-  // create a new tag
+  // * create a new tag
 });
 
 router.put('/:id', async (req, res) => {
   try {
-
+    const tagData = await Tag.findOne({ where: { id: req.params.id},
+    });
+    await tagData.update({ tag_name: req.body.tag_name})
+    res.json(tagData);
 
   } catch (error) {
     console.log(error);
     res.status(400).json(error);
-  }// update a tag's name by its `id` value
+  }// * update a tag's name by its `id` value
 });
 
 router.delete('/:id', async (req, res) => {
   try {
+    await Tag.destroy({
+      where: { id: req.params.id },
+    })
+    res.status(204).send('');  
 
-    
   } catch (error) {
     console.log(error);
     res.status(400).json(error);
